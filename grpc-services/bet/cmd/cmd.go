2483051de8dfd76a801f04e0db6c97ce170a6fc8
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-11-27 15:20:29 +0100

    Use cmd common pkg in bet  service

--- grpc-services/bet/cmd/cmd.go
+++ grpc-services/bet/cmd/cmd.go
@@ -1,62 +1,25 @@
 package cmd
 
 import (
-	"context"
-	"flag"
-	"fmt"
-	"os"
+	commonCmd "github.com/omaressameldin/bet-collector-services/grpc-services/common/pkg/cmd"
 
 	v1 "github.com/omaressameldin/bet-collector-services/grpc-services/bet/pkg/service/v1"
 	"github.com/omaressameldin/bet-collector-services/grpc-services/bet/server"
-	"github.com/omaressameldin/go-database-connector/app/pkg/database"
-	"github.com/omaressameldin/go-database-connector/app/pkg/firebase"
 )
-
-// Config is configuration for Server
-type Config struct {
-	Port           string
-	FirebaseConfig string
-	Collection     string
-}
 
 var v1API *v1.BetServiceServer
 
 // RunServer get the flags and starts the server
 func RunServer() error {
-	ctx := context.Background()
-
-	var cfg Config
-	flag.StringVar(&cfg.Port, "port", "", "port to bind")
-	flag.StringVar(&cfg.FirebaseConfig, "firebaseConfig", "", "firebase json config file")
-	flag.StringVar(&cfg.Collection, "collection", "", "firebase collection")
-	flag.Parse()
-
-	if len(cfg.Port) == 0 {
-		return fmt.Errorf("invalid TCP port for server: '%s'", cfg.Port)
+	ctx, cfg, err := commonCmd.ConfigureFlags()
+	if err != nil {
+		return err
 	}
 
-	if len(cfg.Collection) == 0 {
-		return fmt.Errorf("invalid Collection for firebase database: '%s'", cfg.Collection)
-	}
-
-	_, err := os.Stat(cfg.FirebaseConfig)
-	if os.IsNotExist(err) {
-		return fmt.Errorf("File does not exist: '%s'", cfg.FirebaseConfig)
-	}
-	connector := initConnector(cfg.FirebaseConfig, cfg.Collection)
+	connector := commonCmd.InitFirebaseConnector(cfg.FirebaseConfig, cfg.Collection)
 	v1API = v1.NewBetServiceServer(connector)
 
 	return server.RunServer(ctx, v1API, cfg.Port)
-}
-
-// initConnector initializes database connector
-func initConnector(firebaseConfig, collection string) database.Connector {
-	connector, err := firebase.StartConnection(firebaseConfig, collection, "")
-	if err != nil {
-		panic(err)
-	}
-
-	return connector
 }
 
 // CloseServer closes all connections such as database connection
--- grpc-services/user/go.mod
+++ grpc-services/user/go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.2
+	github.com/omaressameldin/bet-collector-services/grpc-services/common v0.0.0
 	github.com/omaressameldin/go-database-connector/app v0.0.0-20191030104300-016d8084a783
 	google.golang.org/grpc v1.22.3
 )
