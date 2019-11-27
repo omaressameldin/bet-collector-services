004ab7050f46da2668cb3931c0284323f059eb71
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-11-27 15:20:58 +0100

    Use server common pkg in user  service

--- grpc-services/user/main.go
+++ grpc-services/user/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
+	"github.com/omaressameldin/bet-collector-services/grpc-services/common/pkg/server"
 	"github.com/omaressameldin/bet-collector-services/grpc-services/user/cmd"
 )
 
-func exitWithError(err error) {
-	fmt.Fprintf(os.Stderr, "%v\n", err)
-	os.Exit(1)
-}
-
 func close() {
 	if err := cmd.CloseServer(); err != nil {
-		exitWithError(err)
+		server.ExitWithError(err)
 	}
 }
 
@@ -22,6 +15,6 @@ func main() {
 	defer close()
 
 	if err := cmd.RunServer(); err != nil {
-		exitWithError(err)
+		server.ExitWithError(err)
 	}
 }
