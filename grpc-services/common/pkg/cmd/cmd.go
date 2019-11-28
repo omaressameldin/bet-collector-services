4c31b23990228cf349482b625dff8f177530feaf
Omar Essam <o.essameldin@gmail.com>
2019-11-28 15:47:02 +0100

    Parse dependencies flag  if available (#9)

--- grpc-services/common/pkg/cmd/cmd.go
+++ grpc-services/common/pkg/cmd/cmd.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -11,9 +12,11 @@ import (
 )
 
 type Config struct {
-	Port           string
-	FirebaseConfig string
-	Collection     string
+	Port             string
+	FirebaseConfig   string
+	Collection       string
+	Dependencies     map[string]string
+	dependenciesJson string
 }
 
 func ConfigureFlags() (context.Context, *Config, error) {
@@ -23,6 +26,7 @@ func ConfigureFlags() (context.Context, *Config, error) {
 	flag.StringVar(&cfg.Port, "port", "", "port to bind")
 	flag.StringVar(&cfg.FirebaseConfig, "firebaseConfig", "", "firebase json config file")
 	flag.StringVar(&cfg.Collection, "collection", "", "firebase collection")
+	flag.StringVar(&cfg.dependenciesJson, "dependencies", "", "Dependency services urls and names as json")
 	flag.Parse()
 
 	if len(cfg.Port) == 0 {
@@ -33,9 +37,15 @@ func ConfigureFlags() (context.Context, *Config, error) {
 		return nil, nil, fmt.Errorf("invalid Collection for firebase database: '%s'", cfg.Collection)
 	}
 
-	_, err := os.Stat(cfg.FirebaseConfig)
-	if os.IsNotExist(err) {
+	if _, err := os.Stat(cfg.FirebaseConfig); os.IsNotExist(err) {
 		return nil, nil, fmt.Errorf("File does not exist: '%s'", cfg.FirebaseConfig)
+	}
+
+	if len(cfg.dependenciesJson) > 0 {
+		cfg.Dependencies = make(map[string]string)
+		if err := unmarshalDependencies(cfg.dependenciesJson, &cfg.Dependencies); err != nil {
+			panic(err)
+		}
 	}
 
 	return ctx, &cfg, nil
@@ -48,4 +58,14 @@ func InitFirebaseConnector(firebaseConfig, collection string) database.Connector
 	}
 
 	return connector
+}
+
+func unmarshalDependencies(dependenciesJson string, dependencies *map[string]string) error {
+	fmt.Println(dependenciesJson)
+	err := json.Unmarshal([]byte(dependenciesJson), dependencies)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
